cc4c81c72495d365a37ed7c29caacfdd2f630703
Amin Tavakkolnia <62079561+Amin-T@users.noreply.github.com>
2020-07-02 11:44:07 +0200

    Update hadoop-env.cmd

--- hadoop-env.cmd
+++ hadoop-env.cmd
@@ -22,7 +22,7 @@
 @rem remote nodes.
 
 @rem The java implementation to use.  Required.
-set JAVA_HOME=C:\Progra~1\Java\jre1.8.0_251\bin
+set JAVA_HOME=C:\Java\jre1.8.0_251\bin
 
 @rem The jsvc implementation to use. Jsvc is required to run secure datanodes.
 @rem set JSVC_HOME=%JSVC_HOME%
